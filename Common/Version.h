5e676ea1346ee52f62a73b33f0a7d6ede638efd0
kawamoto@tenjin.org <kawamoto@tenjin.org@08c5ca9e-e62b-11dd-b00f-9fabb4b2ff13>
2009-03-08 09:13:17 +0000

    add Version.h.
    git-svn-id: http://iphone-remotepad.googlecode.com/svn/trunk@134 08c5ca9e-e62b-11dd-b00f-9fabb4b2ff13

---
+++ Common/Version.h
@@ -0,0 +1,50 @@
+/*
+ *  Event.h
+ *  RemotePad, RemotePad Server
+ *
+ *  Created by iKawamoto Yosihisa! on 09/03/07.
+ *  Copyright 2009 tenjin.org. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE TENJIN.ORG AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE TENJIN.ORG
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+// version strings
+#define kVersionRemotePad	@"1.4"
+#if (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_5)
+#define kVersionMac			@"2.2alpha1 for Mac OS X 10.5"
+#else
+#define kVersionMac			@"2.2alpha1 for Mac OS X 10.4"
+#endif
+#define kVersionWindows		"1.7"
+#define kVersionX11			"1.7"
+
+// current version
+#define kVersionRemotePadCurrent	0x01010400
+#define kVersionMacCurrent			0x02020101
+#define kVersionX11Current			0x03010700
+#define kVersionWindowsCurrent		0x04010700
+
+// keyboard supported version
+#define kVersionRemotePadKeyboard	0x01010400
+#define kVersionMacKeyboard			0x02020200
+#define kVersionX11Keyboard			0x03010800
+#define kVersionWindowsKeyboard		0x04010800
